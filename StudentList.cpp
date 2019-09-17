3750dcf11358193a25fba9578d0b1f85ef16f71d
U-BSD\411036 <411036@SHS-10DEC4F83.beaverton.k12.or.us>
2019-09-17 09:01:54 -0700

    Added comments, pointer, other

--- StudentList.cpp
+++ StudentList.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//A student struct which contains a firstname, lastname, id, and gpa
 struct Student {
   char firstName[20];
   char lastName[25];
@@ -11,26 +12,31 @@ struct Student {
   float gpa;
 };
 
+//declare functions
 void printHelp();
-bool parse (char c[10], vector<Student>* v);
-void add (vector<Student>* v);
-void del (vector<Student>* v);
+bool parse (char c[10], vector<Student*>* v);
+void add (vector<Student*>* v);
+void del (vector<Student*>* v);
 void quit ();
-void printAll(vector<Student>* v);
+void printAll(vector<Student*>* v);
 
 int main() {
   cout << "Thank you for using StudentList.exe" << endl;
   cout << "Type 'h' for help" << endl;
 
+  //configure cout to print floats to 2 decimal places
   cout.setf(ios::showpoint);
   cout.setf(ios::fixed, ios::floatfield);
   cout.precision(2);
-  
-  vector<Student> studentList;
-  vector<Student>* studentPointer;
 
+  //create the list pointers
+  vector<Student*> studentList;
+  vector<Student*>* studentPointer = &studentList;
+
+  //while running, keep running until parse returns false (quit is called)
   bool running = true;
   while(running){
+    //Get input (command shouldn't be more than 10 chars
     char input[10] = "";
     cin.get(input, 10);
     cin.get();
@@ -41,23 +47,34 @@ int main() {
   return 0;
 }
 
-bool parse(char c[10], vector<Student>* v){
+//Parse function which decides which functions to call and passes necessary arguments
+bool parse(char c[10], vector<Student*>* v){
   for(int i = 0; i < strlen(c); i++){
     c[i] = toupper(c[i]);
   }
+
+  //Print help if H 
   if (strcmp(c, "H") == 0){
     //cout << "HELP" << endl;
     printHelp();
-  } else if(strcmp(c, "ADD") == 0){
+  }
+  //Add student with add
+  else if(strcmp(c, "ADD") == 0){
     //cout << "ADD" << endl;
     add(v);
-  } else if(strcmp(c, "PRINT") == 0){
+  }
+  //Print all students with print
+  else if(strcmp(c, "PRINT") == 0){
     //cout << "PRINT" << endl;
     printAll(v);
-  } else if(strcmp(c, "DELETE") == 0){
+  }
+  //Delete a student with delete
+  else if(strcmp(c, "DELETE") == 0){
     //cout << "DELETE" << endl;
     del(v);
-  } else if(strcmp(c, "QUIT") == 0) {
+  }
+  //Quit program with quit
+  else if(strcmp(c, "QUIT") == 0) {
     //cout << "QUIT" << endl;
     quit();
     return false;
@@ -68,6 +85,7 @@ bool parse(char c[10], vector<Student>* v){
   return true;
 }
 
+//Prints the help manual which consists of all the commands
 void printHelp() {
   cout << "Help Manual:" << endl;
   cout << "Commands: " << endl;
@@ -77,52 +95,80 @@ void printHelp() {
   cout << "QUIT: exit the program" << endl;
 }
 
-void add(vector<Student>* v) {
-  Student newStud;
+//Prompts user for inputs and adds to studentlist
+void add(vector<Student*>* v) {
+  Student *newStud = new Student();
+
+  //Prompt user for first, last, id, and gpa
   cout << "Please enter the student's first name" << endl;
-  cin >> newStud.firstName;
+  cin >> (*newStud).firstName;
   cout << "Please enter the student's last name" << endl;
-  cin >> newStud.lastName;
+  cin >> (*newStud).lastName;
   cout << "Please enter the student's ID" << endl;
-  cin >> newStud.id;
+  cin >> (*newStud).id;
   cout << "Please enter the student's GPA" << endl;
-  cin >> newStud.gpa;
-  (*v).push_back(newStud);
-  //cin.clear();
-  cin.get();
-  cout << "Student " << newStud.firstName << " " << newStud.lastName << " has been added to the list." << endl;
-}
-
-void printAll(vector<Student>* v){
-  vector<Student>::iterator it;
+  cin >> (*newStud).gpa;
   
-  for (it = (*v).begin(); it != (*v).end(); it++) {
-    cout << (*it).firstName << " " << (*it).lastName << ": " << (*it).id << ", " << (*it).gpa << endl; 
-  }
+  //Add student to list
+  (*v).push_back(newStud);
+
+  //Clear newline from input
+  cin.get();
+
+  //Tell user they added a student to the list
+  cout << "Student " << (*newStud).firstName << " " << (*newStud).lastName << " has been added to the list." << endl;
 }
 
+//Prints students in list
+void printAll(vector<Student*>* v){
+  bool printed = false;
+
+  //iterator for vector
+  vector<Student*>::iterator it;
+
+  //iterate through vector
+  for (it = (*v).begin(); it != (*v).end(); ++it) {
+    printed = true;
+
+    //Print the student with formatting
+    cout << (**it).firstName << " " << (**it).lastName << ": " << (**it).id << ", " << (**it).gpa << endl; 
+  }
+
+  //If no students were printed say so
+  if(!printed) cout << "There are no students in the list." << endl;
+}
+
+//Prints exit message (add other exit functions here)
 void quit() {
   cout << "Thank you for using StudentList! Have a great day!" << endl;
 }
 
-void del(vector<Student>* v) {
+//Deletes given student
+void del(vector<Student*>* v) {
   char first[20];
   char last[20];
+
+  //Prompt user for first and last name
   cout << "Enter the student name (First and Last)" << endl;
   cin >> first >> last;
   cin.get();
-  vector<Student>::iterator it;
-
+  vector<Student*>::iterator it;
   bool removed = false;
+
+  //iterate through vector
   for (it = (*v).begin(); it != (*v).end(); it++){
-    if (strcmp((*it).firstName, first) == 0 && strcmp((*it).lastName, last) == 0) {
+    if (strcmp((**it).firstName, first) == 0 && strcmp((**it).lastName, last) == 0) {
+      //Remove student from list
       (*v).erase(it);
       removed = true;
       cout << "Removed Student " << first << " " << last << " from list." << endl;
+      break;
     }
   }
+  
+  //If no students were removed say so
   if(!removed){
     cout << "Could not find any students with name " << first << " " << last << " in list." << endl;
   }
 }
-  
+
